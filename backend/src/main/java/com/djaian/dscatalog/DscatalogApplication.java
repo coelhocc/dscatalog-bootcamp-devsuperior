b1b8f8393ffc4277f4d49f9ce896eeb35326d22a
Jean Coelho <coelhocc@gmail.com>
2020-09-29 20:31:14 -0300

    Project created

---
+++ backend/src/main/java/com/djaian/dscatalog/DscatalogApplication.java
@@ -0,0 +1,13 @@
+package com.djaian.dscatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscatalogApplication.class, args);
+	}
+
+}
