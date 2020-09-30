9af3683e4c0878ce0253bcbd32858996c9e61d87
Jean Coelho <coelhocc@gmail.com>
2020-09-29 21:41:15 -0300

    DTO

--- backend/src/main/java/com/djaian/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/djaian/dscatalog/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.djaian.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.djaian.dscatalog.dto.CategoryDTO;
 import com.djaian.dscatalog.entities.Category;
 import com.djaian.dscatalog.repositories.CategoryRepository;
 
@@ -16,7 +18,10 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<Category> findAll() {
-		return repository.findAll();
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		
 	}
 }
