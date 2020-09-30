73662ad76e65eefa73907066f2daae34a4151e8a
Jean Coelho <coelhocc@gmail.com>
2020-09-29 21:20:51 -0300

    H2 database

---
+++ backend/src/main/java/com/djaian/dscatalog/services/CategoryService.java
@@ -0,0 +1,20 @@
+package com.djaian.dscatalog.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.djaian.dscatalog.entities.Category;
+import com.djaian.dscatalog.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+}
