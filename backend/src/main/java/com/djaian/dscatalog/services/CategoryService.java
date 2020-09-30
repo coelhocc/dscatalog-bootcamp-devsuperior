cffefb4adb8defc97f5e82861f02216baea29bf5
Jean Coelho <coelhocc@gmail.com>
2020-09-30 20:48:08 -0300

    Delete Category

--- backend/src/main/java/com/djaian/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/djaian/dscatalog/services/CategoryService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.djaian.dscatalog.dto.CategoryDTO;
 import com.djaian.dscatalog.entities.Category;
 import com.djaian.dscatalog.repositories.CategoryRepository;
+import com.djaian.dscatalog.services.exceptions.DatabaseException;
 import com.djaian.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -55,5 +58,18 @@ public class CategoryService {
 		catch(EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found " + id);
 		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
+		
 	}
 }
