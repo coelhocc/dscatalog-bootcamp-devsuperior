cca38775af7636ac58cf17bf0ae2c316c44afaf8
Jean Coelho <coelhocc@gmail.com>
2020-10-02 00:08:42 -0300

    Product repositoy, service, resource, GET ok

---
+++ backend/src/main/java/com/djaian/dscatalog/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.djaian.dscatalog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.djaian.dscatalog.entities.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
