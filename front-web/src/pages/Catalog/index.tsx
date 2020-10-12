54c7e687cad5455ff3300aa06acdc5a3d18fd53c
Jean Coelho <coelhocc@gmail.com>
2020-10-12 20:56:19 -0300

    Finish catalog loaders

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -2,12 +2,14 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import ProductCard from './components/ProductCard';
 import './styles.scss';
-import { makeRequest } from '../../core/utils/request';
-import { ProductsResponse } from '../../core/types/Product';
+import { makeRequest } from 'core/utils/request';
+import { ProductsResponse } from 'core/types/Product';
+import ProductCardLoader from './components/Loaders/ProductCardLoader';
 
 const Catalog = () => {
 
   const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
+  const [isLoading, setIsLoading] = useState(false);
 
   //Através do userEffect é possívell acessar o ciclo de vida do componente.
   useEffect(() => {
@@ -16,8 +18,12 @@ const Catalog = () => {
       linesPerPage: 12
     }
 
+    setIsLoading(true);
     makeRequest({ url: '/products', params })
-    .then(response => setProductsResponse(response.data));
+    .then(response => setProductsResponse(response.data))
+    .finally(() => {
+      setIsLoading(false);
+    });
   }, []);
 
   return (
@@ -26,11 +32,12 @@ const Catalog = () => {
         Catalogo de produtos
       </h1>
       <div className="catalog-products">
-        {productsResponse?.content.map(product => (
+        {isLoading ? <ProductCardLoader /> : (
+          productsResponse?.content.map(product => (
           <Link to={`/products/${product.id}`} key={product.id}>
             <ProductCard product={product}/>
           </Link>
-        ))}
+        )))}
       </div>
     </div>
   );
