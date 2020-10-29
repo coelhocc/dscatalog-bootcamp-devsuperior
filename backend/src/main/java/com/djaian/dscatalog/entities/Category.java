27e784403dd6e1a3d6bf31a4b329c43edad837fc
Jean Coelho <coelhocc@gmail.com>
2020-10-28 22:37:41 -0300

    ORM mapping, database seeding

--- backend/src/main/java/com/djaian/dscatalog/entities/Category.java
+++ backend/src/main/java/com/djaian/dscatalog/entities/Category.java
@@ -2,12 +2,15 @@ package com.djaian.dscatalog.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
@@ -27,6 +30,9 @@ public class Category implements Serializable{
 
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant updatedAt;
+	
+	@ManyToMany(mappedBy = "categories")
+	private Set<Product> produtcs = new HashSet<>();
 	
 	public Category() {
 	}
@@ -69,7 +75,11 @@ public class Category implements Serializable{
 	public void preUpdate() {
 		updatedAt = Instant.now();
 	}
-	
+		
+	public Set<Product> getProdutcs() {
+		return produtcs;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
